5a5f19ad0fc245c56b1717c30305fcf8df9f8cbb
Dallin Wardrop <dallinwardrop12@gmail.com>
2016-06-17 16:56:20 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,18 @@
 #write your code here
 
-def countdown (a1)
-  x = 10
-    while x <= 10
-      puts "#{x}SECOND(S)!"
-      x -= 1
-    end
-  puts "HAPPY NEW YEAR!"
+def countdown (seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    seconds_to_midnight -= 1
+  end
+  seconds_to_midnight.to_s + " left. HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    sleep(1)
+    seconds_to_midnight -= 1
+  end
+  seconds_to_midnight.to_s + "left. HAPPY NEW YEAR!"
 end
